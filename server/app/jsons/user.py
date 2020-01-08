a65448d971fb7166f3bbb59c6caecd18a4cbc9dc
Mohammad Javad <mjshz.73@gmail.com>
2020-01-08 17:07:21 +0330

    some changs

--- server/app/jsons/user.py
+++ server/app/jsons/user.py
@@ -4,13 +4,3 @@ from jsl import Document, StringField
 class User(Document):
     name = StringField()
     family = StringField()
-
-
-class AHMAGhUser(Document):
-    name = StringField()
-    family = StringField()
-
-class AliUser(Document):
-    name = StringField()
-    family = StringField()
-
--- server/app/models/user.py
+++ server/app/models/user.py
@@ -1,5 +1,19 @@
 from mongoengine import *
+from flask import request
+from server.app.extensions import redis
 
 class User(Document):
     name = StringField()
     family = StringField()
+
+    @classmethod
+    def check_user(cls):
+        access_token = request.headers.get('Access-Token', None)
+        if not access_token:
+            return False
+        
+        user_id = redis.get('uat%s' % access_token, None)
+        if not user_id:
+            return False
+
+        user = cls.objects.get(id=user_id)
